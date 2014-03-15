326fbc5e297694aed043ffc8ae74fd98aca4707f
EnolEther <jjmakis@gmail.com>
2014-03-15 01:51:03 -0500

    Routing updated to link to new notes page

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Mywebsite::Application.routes.draw do
   match '/about', to: "static_pages#about", via: 'get'
   match '/photography', to: "photography_pages#photography", via: 'get'
   match '/contact', to: "static_pages#contact", via: 'get'
-  # match '/notes', to: "static_pages#notes", via: 'get'
   match '/notes', to: "markdown_pages#myNotes", via: 'get'
   
   # The priority is based upon order of creation: first created -> highest priority.
