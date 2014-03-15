d6b5048851ea8b626a8044fe2dd81d34ef43bb32
EnolEther <jjmakis@gmail.com>
2014-03-14 22:58:19 -0500

    Migrated to rdiscount gem and moved content

--- app/helpers/markdown_pages_helper.rb
+++ app/helpers/markdown_pages_helper.rb
@@ -1,2 +1,16 @@
 module MarkdownPagesHelper
+  require 'rdiscount'
+
+  def markdown_read(fname)
+    File.read(Rails.root+"app/assets/markdown/"+fname)
+  end
+
+  def markdown_readmeFile
+    File.read("README.md")
+  end
+
+  def renderMarkdown(text)
+    markdown = RDiscount.new(text, :autolink, :smart)
+    return markdown.to_html.html_safe
+  end
 end
