168510824531c6edce463788e857790a28e363b1
EnolEther <jjmakis@gmail.com>
2014-03-20 15:31:32 -0500

    improved flickr api integration

--- app/controllers/photography_pages_controller.rb
+++ app/controllers/photography_pages_controller.rb
@@ -1,47 +1,47 @@
 class PhotographyPagesController < ApplicationController
 
-  # This is the URL Flickr will redirect your users to once they agree to access
-  @callback_url='http://localhost:3000/auth_controller/callback'
+  # # This is the URL Flickr will redirect your users to once they agree to access
+  # @callback_url='http://localhost:3000/auth_controller/callback'
 
-  FlickRaw.api_key=ENV['API_KEY']
-  FlickRaw.shared_secret=ENV['SHARED_SECRET']
+  # FlickRaw.api_key=ENV['API_KEY']
+  # FlickRaw.shared_secret=ENV['SHARED_SECRET']
 
   def photography
     @myPhotos = flickr.photos.search(:user_id => '56381296@N00', :per_page => 20)
   end
 
   # Users should hit this method to get the link which sends them to flickr
-  def auth
-    flickr = FlickRaw::Flickr.new
-    token = flickr.get_request_token(:oauth_callback => URI.escape(@callback_url))
-    # You'll need to store the token somewhere for when the user is returned to the callback method
-    # I stick mine in memcache with their session key as the cache key
+  # def auth
+  #   flickr = FlickRaw::Flickr.new
+  #   token = flickr.get_request_token(:oauth_callback => URI.escape(@callback_url))
+  #   # You'll need to store the token somewhere for when the user is returned to the callback method
+  #   # I stick mine in memcache with their session key as the cache key
 
-    @auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
-    # Stick @auth_url in your template for users to click
-  end
+  #   @auth_url = flickr.get_authorize_url(token['oauth_token'], :perms => 'delete')
+  #   # Stick @auth_url in your template for users to click
+  # end
 
   # Your users browser will be redirected here from Flickr (see @callback_url above)
-  def callback
-    flickr = FlickRaw::Flickr.new
+  # def callback
+  #   flickr = FlickRaw::Flickr.new
 
-    request_token = # Retrieve from cache or session etc - see above
-    oauth_token = params[:oauth_token]
-    oauth_verifier = params[:oauth_verifier]
+  #   request_token = # Retrieve from cache or session etc - see above
+  #   oauth_token = params[:oauth_token]
+  #   oauth_verifier = params[:oauth_verifier]
 
-    raw_token = flickr.get_access_token(request_token['oauth_token'], request_token['oauth_token_secret'], oauth_verifier)
-    # raw_token is a hash like this {"user_nsid"=>"92023420%40N00", "oauth_token_secret"=>"XXXXXX", "username"=>"boncey", "fullname"=>"Darren%20Greaves", "oauth_token"=>"XXXXXX"}
-    # Use URI.unescape on the nsid and name parameters
+  #   raw_token = flickr.get_access_token(request_token['oauth_token'], request_token['oauth_token_secret'], oauth_verifier)
+  #   # raw_token is a hash like this {"user_nsid"=>"92023420%40N00", "oauth_token_secret"=>"XXXXXX", "username"=>"boncey", "fullname"=>"Darren%20Greaves", "oauth_token"=>"XXXXXX"}
+  #   # Use URI.unescape on the nsid and name parameters
 
-    oauth_token = raw_token["oauth_token"]
-    oauth_token_secret = raw_token["oauth_token_secret"]
+  #   oauth_token = raw_token["oauth_token"]
+  #   oauth_token_secret = raw_token["oauth_token_secret"]
 
-    # Store the oauth_token and oauth_token_secret in session or database
-    #   and attach to a Flickraw instance before calling any methods requiring authentication
+  #   # Store the oauth_token and oauth_token_secret in session or database
+  #   #   and attach to a Flickraw instance before calling any methods requiring authentication
 
-    # Attach the tokens to your flickr instance - you can now make authenticated calls with the flickr object
-    flickr.access_token = oauth_token
-    flickr.access_secret =[oauth_token_secret]
+  #   # Attach the tokens to your flickr instance - you can now make authenticated calls with the flickr object
+  #   flickr.access_token = oauth_token
+  #   flickr.access_secret =[oauth_token_secret]
 
-  end
+  # end
 end
--- app/helpers/photography_pages_helper.rb
+++ app/helpers/photography_pages_helper.rb
@@ -8,45 +8,75 @@ module PhotographyPagesHelper
     return FlickRaw.url_q(photo_info)
   end
 
-  def getOrginalUrl (photo_info)
-    return FlickRaw.url_o(photo_info)
-  end
+  # def getThumbURL (photo_info)
+  #   return FlickRaw.url_t(photo_info)
+  # end
 
-  def getMediumUrl (photo_info)
-    return FlickRaw.url_z(photo_info)
-  end
+  # def getOrginalURL (photo_info)
+  #   return FlickRaw.url_o(photo_info)
+  # end
 
-  def getLargeUrl (photo_info)
-    return FlickRaw.url_b(photo_info)
-  end
+  # def getMediumUrl (photo_info)
+  #   return FlickRaw.url_z(photo_info)
+  # end
+
+  # def getLargeUrl (photo_info)
+  #   return FlickRaw.url_b(photo_info)
+  # end
 
   def getPhotoTitle (photo_info)
     return photo_info.title
+  end
+
+  def getLargePhotoURL (photo_element)
+    sizes = flickr.photos.getSizes(:photo_id => photo_element.id)
+    original = sizes.find {|s| s.label == 'Original' }
+    large = sizes.find {|s| s.label == 'Large'}
+    if large == nil
+      return  original.source
+    else
+      return large.source
+    end
   end
 
   def parsePhotoData (photo_elements)
     theData = Array.new
     photo_elements.each do |p|
       info = getPhotoInfo(p)
-      surl = getSquareUrl(info)
-      lgurl = getLargeUrl(info)
+      thumbURL = getSquareUrl(info)
+      largeURL = getLargePhotoURL(p)
       title = getPhotoTitle(info)
-      theHash = {:title => title, :sqURL => surl, :lgURL => lgurl}
+      theHash = {:title => title, :thumbURL => thumbURL, :largeURL => largeURL}
       theData.push theHash
     end
     return theData
   end
 
-  def makeTheHTML(theData)
-    theHTML = Array.new
-    theData.each do |ele|
-      str = "<img src='#{ele[:sqURL]}' alt='#{ele[:title]}' >"
-      theHTML.push str
-    end
-    return theHTML
-  end
+#   def parsePhotoDataOLD (photo_elements)
+#     theData = Array.new
+#     photo_elements.each do |p|
+#       info = getPhotoInfo(p)
+#       surl = getSquareUrl(info)
+#       ourl = getOrginalUrl(info)
+#       lgurl = getLargeUrl(info)
+#       turl = getPhotoSize(p)
+#       title = getPhotoTitle(info)
+#       theHash = {:title => title, :sqURL => surl, :srcURL => turl, :lgURL => lgurl, :oURL => ourl}
+#       theData.push theHash
+#     end
+#     return theData
+#   end
 
-  def toPage(list)
-    makeTheHTML( parsePhotoData(list) )
-  end
+#   def makeTheHTML(theData)
+#     theHTML = Array.new
+#     theData.each do |ele|
+#       str = "<img src='#{ele[:sqURL]}' alt='#{ele[:title]}' >"
+#       theHTML.push str
+#     end
+#     return theHTML
+#   end
+
+#   def toPage(list)
+#     makeTheHTML( parsePhotoData(list) )
+#   end
 end
