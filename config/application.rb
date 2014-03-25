bec67652f472b6f744e211fb65302083c3300a10
EnolEther <jjmakis@gmail.com>
2014-03-25 14:50:44 -0500

    trying to fix javascript errors on heroku

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Mywebsite
     # config.assets.initialize_on_precompile = false
 
     # Disable asset precompile
-    config.assets.enabled = []
+    # config.assets.enabled = []
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
