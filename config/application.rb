b4d354fdffce4bb68398e4c0e77788185333b038
EnolEther <jjmakis@gmail.com>
2014-03-25 14:39:04 -0500

    trying to fix javascript errors

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,9 @@ module Mywebsite
     # -- all .rb files in that directory are automatically loaded.
     # config.assets.initialize_on_precompile = false
 
+    # Disable asset precompile
+    config.assets.enabled = false
+
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
